df0b50e905fced93dea5a0fa148bcdebcd449e17
Bharathi172517 <bharathikamireddy7@gmail.com>
2026-05-18 07:25:28 +0530

    conversation

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -10,3 +10,6 @@ echo "additon of $NUMBER1 and $NUMBER2 is: $SUM"
 TIMESTAMP=$(date)
 
 echo "this script executed at: $TIMESTAMP"
+
+SUM1=$(($NUMBER1+$NUMBER2))
+echo "additon of $NUMBER1 and $NUMBER2 is: $SUM1"
