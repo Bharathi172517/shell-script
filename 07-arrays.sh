4a41185876283f91bba39598f5c3435e639d43be
Bharathi172517 <bharathikamireddy7@gmail.com>
2026-05-19 22:35:34 +0530

    conversation

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("anam" "kanam" "yanam")
+
+echo "First movie:${MOVIES[0]}"
+echo "second movie:${MOVIES[1]}"
+echo "third movie:${MOVIES[3]}"
+
+echo "all movies are:${MOVIES[@]}"
