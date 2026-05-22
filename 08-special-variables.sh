eb9f1d488f17975967fea20852d05002fa7baf55
Bharathi172517 <bharathikamireddy7@gmail.com>
2026-05-22 08:23:59 +0530

    conversation

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -5,7 +5,7 @@ echo "first variable:$1"
 echo "number of varables:$#"
 echo "script name:$0"
 echo "present working directory:$PWD"
-echo "home directory of current user:$USER"
+echo "home directory of current user:$HOME"
 echo "which user s running script:$USER"
 echo "process id of current script:$$" 
 sleep 60 &
