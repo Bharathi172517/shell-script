3d6e3b72c25764726491da55ccb1a67b6e7089f4
Bharathi172517 <bharathikamireddy7@gmail.com>
2026-05-30 17:27:44 +0530

    conversation

--- 10-installmysql.sh
+++ 10-installmysql.sh
@@ -7,4 +7,36 @@ if [ $USERID -ne 0 ]
   exit 1
 fi
 
-dnf install mysql -y
+dnf list installed mysql
+if [ $? -ne 0 ]
+ then 
+    dnf install mysql -y
+      if [ $? -ne 0 ]
+      then 
+          echo "mysql installaton ---failed"
+          exit 1
+      else
+          echo "mysql installaton ---success"
+
+        fi
+  else
+     echo "mysql already installed"
+  fi
+
+
+ dnf list installed git
+if [ $? -ne 0 ]
+ then 
+    dnf install git -y
+      if [ $? -ne 0 ]
+      then 
+          echo "git installaton ---failed"
+          exit 1
+      else
+          echo "git installaton ---success"
+
+        fi
+  else
+     echo "git already installed"
+  fi
+                     
