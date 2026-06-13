693c74c4b525c9833c94544443670f9acac491b8
Bharathi172517 <bharathikamireddy7@gmail.com>
2026-06-13 16:35:30 +0530

    conversation

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for i in {0..1000}
-do
-echo $i
-done
+# for i in {0..1000}
+# do
+# echo $i
+# done
 
 
 # echo 1
@@ -10,3 +10,45 @@ done
 # echo 3
 # echo 4
 # echo 5
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$( echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+      then 
+          echo -e "$2 --- $R failed $N"
+          exit 1
+      else
+          echo -e "$2 ---$G success"
+
+        fi
+}
+
+echo "script started executing at : $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+ then 
+  echo "error: must have root access"
+  exit 1
+fi
+
+for package in $@
+ do
+   dnf list installed $package &>>$LOG_FILE_NAME
+    if [$? -ne 0]
+         then 
+            dnf  install $package &>>$LOG_FILE_NAME
+            VALIDATE $? "installing $package"
+        else
+           echo -e "$package already $Y installed $N"
+    fi 
+done          
